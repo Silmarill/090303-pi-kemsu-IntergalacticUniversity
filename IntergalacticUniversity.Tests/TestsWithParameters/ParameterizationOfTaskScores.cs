54ef204f816903bd4125049a3da83c474d889b35
Konstantin <H1dra5@yandex.ru>
2026-05-19 03:42:57 +0700

    Сделал 2.1, и почти 2.2

---
+++ IntergalacticUniversity.Tests/TestsWithParameters/ParameterizationOfTaskScores.cs
@@ -0,0 +1,43 @@
+﻿//16 строку помогал писать ИИ
+
+
+using IntergalacticUniversity.Core.Interfaces;
+using IntergalacticUniversity.Core.Models;
+using IntergalacticUniversity.Core.Services;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticUniversity.Tests.TestsWithParameters {
+  public class ParameterizationOfTaskScores {
+    private RatingCalculator _calculator;
+    private Mock<IAttendanceRepository> _mockAttendance;
+    private Student _student;
+    private Course _course;
+
+    [SetUp]
+    public void SetUp() {
+      _calculator = new RatingCalculator(null, null);
+      _mockAttendance = new Mock<IAttendanceRepository>();
+      _mockAttendance.Setup(r => r.GetAttendedClasses(_student, _course)).Returns(_course.TotalClasses);
+
+      _course = new Course {
+        Type = ExamType.Exam,
+        MaxRawAssignmentsScore = 1000,
+        TotalClasses = 40,
+        MaxAttendanceScore = 20
+      };
+    }
+
+    [TestCase(0,0)]
+    [TestCase(300,12)]
+    [TestCase(1000,40)]
+ 
+    public void TaskScore(int score, int expected) {
+      int result = _calculator.CalculateCurrentScore(score,expected);
+      Assert.That(result, Is.EqualTo(expected));
+    }
+  }
+}
---
+++ IntergalacticUniversity.Tests/TestsWithParameters/TranslationBoundary.cs
@@ -0,0 +1,32 @@
+﻿//Сделал ИИ
+
+using IntergalacticUniversity.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticUniversity.Tests.TestsWithParameters {
+  public class TranslationBoundary {
+    private RatingCalculator _calculator;
+
+    [SetUp]
+    public void SetUp() {
+      _calculator = new RatingCalculator(null, null);
+    }
+
+    [TestCase(49, "Неудовлетворительно")]
+    [TestCase(51, "Удовлетворительно")]
+    [TestCase(60, "Удовлетворительно")]
+    [TestCase(66, "Хорошо")]
+    [TestCase(75, "Хорошо")]
+    [TestCase(86, "Отлично")]
+    [TestCase(100, "Отлично")]
+
+    public void ConvertToGrade_OnBoundaryValues_ReturnsCorrectGrade(int score, string expected) {
+      string result = _calculator.ConvertToGrade(score);
+      Assert.That(result, Is.EqualTo(expected));
+    }
+  }
+}
