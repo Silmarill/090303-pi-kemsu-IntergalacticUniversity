1abc3ef15e2c0c742026e1de31ca2fabfdbfe4b9
dezue <mistfoll@mail.ru>
2026-05-19 15:00:03 +0700

    part 2.1

--- IntergalacticUniversity.Tests/ParametrizedTests/GradeConversionTests.cs
+++ IntergalacticUniversity.Tests/ParametrizedTests/GradeConversionTests.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IntergalacticUniversity.Core.Services;
 
-namespace IntergalacticUniversity.Tests.ParametrizedTests {
-  internal class GradeConversionTests {
+namespace IntergalacticUniversity.Tests {
+  [TestFixture]
+  public class GradeConversionTests {
+    private RatingCalculator _calculator;
+
+    [SetUp]
+    public void SetUp() {
+      _calculator = new RatingCalculator(null, null);
+    }
+
+    [TestCase(49, "Неудовлетворительно")]
+    [TestCase(51, "Удовлетворительно")]
+    [TestCase(60, "Удовлетворительно")]
+    [TestCase(66, "Хорошо")]
+    [TestCase(75, "Хорошо")]
+    [TestCase(86, "Отлично")]
+    [TestCase(100, "Отлично")]
+    public void ConvertToGrade_VariousScores_ReturnsExpectedGrade(double totalScore, string expectedGrade) {
+      string result = _calculator.ConvertToGrade(totalScore);
+      Assert.That(result, Is.EqualTo(expectedGrade));
+    }
   }
 }
