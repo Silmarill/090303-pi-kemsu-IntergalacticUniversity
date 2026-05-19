86691d545a7e310f05d772c3deaf91784dca8fa9
Konstantin <H1dra5@yandex.ru>
2026-05-19 14:21:31 +0700

    Доделал 2.2

--- IntergalacticUniversity.Tests/TestsWithParameters/ParameterizationOfTaskScores.cs
+++ IntergalacticUniversity.Tests/TestsWithParameters/ParameterizationOfTaskScores.cs
@@ -1,27 +1,21 @@
 ﻿//Почти всё сделано ИИ
 
-
 using IntergalacticUniversity.Core.Interfaces;
 using IntergalacticUniversity.Core.Models;
 using IntergalacticUniversity.Core.Services;
 using Moq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IntergalacticUniversity.Tests.TestsWithParameters {
   public class ParameterizationOfTaskScores {
     private RatingCalculator _calculator;
     private Mock<IAttendanceRepository> _mockAttendance;
+    private Mock<IAssignmentsRepository> _mockAssignments;
     private Student _student;
     private Course _course;
 
     [SetUp]
     public void SetUp() {
-      _calculator = new RatingCalculator(null, null);
-      _mockAttendance = new Mock<IAttendanceRepository>();
-      _mockAttendance.Setup(r => r.GetAttendedClasses(_student, _course)).Returns(_course.TotalClasses);
+      _student = new Student { Id = 1 };
 
       _course = new Course {
         Type = ExamType.Exam,
@@ -29,15 +23,26 @@ namespace IntergalacticUniversity.Tests.TestsWithParameters {
         TotalClasses = 40,
         MaxAttendanceScore = 20
       };
+
+      _mockAttendance = new Mock<IAttendanceRepository>();
+      _mockAttendance.Setup(r => r.GetAttendedClasses(_student, _course)).Returns(_course.TotalClasses);
+
+      _mockAssignments = new Mock<IAssignmentsRepository>();
+
+      _calculator = new RatingCalculator(_mockAttendance.Object, _mockAssignments.Object);
     }
 
-    [TestCase(0,0)]
-    [TestCase(300,12)]
-    [TestCase(1000,40)]
- 
-    public void TaskScore(int score, int expected) {
-      int result = _calculator.CalculateCurrentScore(score,expected);
-      Assert.That(result, Is.EqualTo(expected));
+    [TestCase(0, 0)]
+    [TestCase(300, 12)]
+    [TestCase(1000, 40)]
+    public void TaskScore(int rawScore, int expectedAssignmentsScore) {
+      _mockAssignments.Setup(r => r.GetRawScore(_student, _course)).Returns(rawScore);
+
+      double current = _calculator.CalculateCurrentScore(_student, _course);
+
+      double actualAssignmentsScore = current - 20;
+
+      Assert.That(actualAssignmentsScore, Is.EqualTo(expectedAssignmentsScore));
     }
   }
 }
--- IntergalacticUniversity.Tests/TestsWithParameters/TranslationBoundary.cs
+++ IntergalacticUniversity.Tests/TestsWithParameters/TranslationBoundary.cs
@@ -1,7 +1,6 @@
 ﻿//Сделал ИИ
 
 using IntergalacticUniversity.Core.Services;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
