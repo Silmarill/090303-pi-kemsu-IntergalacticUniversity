76ff8cb0f80d81d1891b72e65c2d577935b3c7b2
dezue <mistfoll@mail.ru>
2026-05-19 17:18:05 +0700

    part 3.4

--- IntergalacticUniversity.Tests/MockInteractionTests/ExceptionPropagationTests.cs
+++ IntergalacticUniversity.Tests/MockInteractionTests/ExceptionPropagationTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using IntergalacticUniversity.Core.Models;
+using IntergalacticUniversity.Core.Interfaces;
+using IntergalacticUniversity.Core.Services;
 
-namespace IntergalacticUniversity.Tests.MockInteractionTests {
-  internal class ExceptionPropagationTests {
+namespace IntergalacticUniversity.Tests {
+  [TestFixture]
+  public class ExceptionPropagationTests {
+    private Student _student;
+    private Course _course;
+    private Mock<IAttendanceRepository> _mockAttendance;
+    private Mock<IAssignmentsRepository> _mockAssignments;
+    private RatingCalculator _calculator;
+
+    [SetUp]
+    public void SetUp() {
+      _student = new Student { Id = 1, Name = "Тестовый Студент" };
+      _course = new Course {
+        CourseId = 1,
+        Name = "Экзаменационный курс",
+        Type = ExamType.Exam,
+        MaxRawAssignmentsScore = 800,
+        TotalClasses = 40,
+        MaxAttendanceScore = 20
+      };
+      _mockAttendance = new Mock<IAttendanceRepository>();
+      _mockAssignments = new Mock<IAssignmentsRepository>();
+    }
+
+    [Test]
+    public void CalculateCurrentScore_WhenRepositoryThrowsException_PropagatesException() {
+      _mockAssignments.Setup(r => r.GetRawScore(_student, _course)).Throws<TimeoutException>();
+      _mockAttendance.Setup(r => r.GetAttendedClasses(_student, _course)).Returns(40);
+      _calculator = new RatingCalculator(_mockAttendance.Object, _mockAssignments.Object);
+
+      Assert.Throws<TimeoutException>(() => _calculator.CalculateCurrentScore(_student, _course));
+    }
   }
 }
